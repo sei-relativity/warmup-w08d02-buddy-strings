cedc4a67d5c302bccf00c142121765fd1999f5d9
osama <>
2019-11-18 09:57:22 +0300

    Osama Alruthaya - Silver Blade

---
+++ app.rb
@@ -0,0 +1,20 @@
+def swap (a , b)
+    a = a.gsub(/\s+/, "")
+    (0...a.size-1).each do |i|
+      a[i],a[i+1] = a[i+1],a[i]
+      if a == b
+        puts "#{a}, #{b}"
+        puts 'true'
+        return true
+      else
+        a[i],a[i+1] = a[i+1],a[i]
+      end
+    end
+    puts "#{a}, #{b}"
+    puts 'false'
+end
+
+swap'aaa  aaaabc', 'aaaaaaacb'
+swap'aaaaaaabc', 'aaaaaaacb'
+swap ' ', 'aaaaaaacb'
+swap'cb', 'cb'
